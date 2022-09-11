776102dc50416208643f1f71ac8190771714d0e9
Shanbashir1 <shanbashir1@hotmail.com>
2022-09-11 08:59:25 +0000

    Initial commit and created project structure, adding creds JSON file and google sheets

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love-pizza')
+
+#Introducing the Customer and a Welcome Message.  
+userName = input('Please enter your name: ')
+welcomeMessage = f'Welcome to Love Pizza, Please may we take your order {userName}'
+print(userName)
+print(welcomeMessage)
+
