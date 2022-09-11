7bf82d2e726015727bbd78ad767d80575d2ff5c3
Shanbashir1 <shanbashir1@hotmail.com>
2022-09-11 11:43:25 +0000

    linked the worksheet and tested it to see if data is being received

--- run.py
+++ run.py
@@ -10,11 +10,18 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love-pizza')
+SHEET = GSPREAD_CLIENT.open('love_pizza')
 
 #Introducing the Customer and a Welcome Message.  
-userName = input('Please enter your name: ')
-welcomeMessage = f'Welcome to Love Pizza, Please may we take your order {userName}'
-print(userName)
-print(welcomeMessage)
+#userName = input('Please enter your name: ')
+#welcomeMessage = f'Welcome to Love Pizza, Please may we take your order {userName}'
+#print(userName)
+#print(welcomeMessage)
 
+#read data from file 
+
+order_list = SHEET.worksheet('order_list')
+
+data = order_list.get_all_values()
+
+print(data)
