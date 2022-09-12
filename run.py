fa4314b057877e29a658cecceaae04fdc6a193ef
Shanbashir1 <shanbashir1@hotmail.com>
2022-09-12 11:29:51 +0000

    Added while loops for user to quit ordering if they want to quit

--- run.py
+++ run.py
@@ -15,17 +15,17 @@ SHEET = GSPREAD_CLIENT.open('love_pizza')
 """
 Welcoming user with name input and a welcome message
 """
-dataAvailableDict = {}
-userName = input('Please enter your name: ')
-addressUser = input('Please enter your house number followed by your firstline of address: ')
-postcodeUser = input('Please enter your postcode: ')
-welcomeMessage = f'Welcome to Love Pizza, Please may we take your order {userName}'
-print(userName)
-print(addressUser, postcodeUser)
-print('*'*60)
-print(welcomeMessage)
-print('*'*60)
 
+userName = input('Please enter your name: ')
+
+addressUser = input('Please enter your house number followed by your firstline of address: ')
+
+postcodeUser = input('Please enter your postcode: ')
+
+print('*'*65)
+print(f'Welcome {userName} to our Love Pizza Store. We make fresh, tasty pizza' )
+print(f'Delivery Address: {addressUser} {postcodeUser}')
+print('*'*65)
 
 """
 Linking worksheet to program and displaying menu for user
@@ -42,12 +42,15 @@ for pizza in data:
 """
 Prompting user option to place order and selecting which Pizza they require
 """
-noPurchase = input('Would you like to continue ordering? (yes/no): ')
-if noPurchase.lower()=='yes':
-
-    pass
-else: 
-    print('Sorry you did not like our selection of our delicious freshly made Pizza , maybe next time') 
-
-
-
+while True:
+    noPurchase = input('Would you like to continue ordering? (yes/no): ')
+    if noPurchase.lower()=='yes':
+        # call function to order pizza
+        break
+    elif noPurchase.lower()=='no':
+        print('Sorry you did not like our selection of our delicious freshly made Pizza , maybe next time')
+        # function to close 
+        quit()
+    else:
+        print('Invalid Input you must enter yes / no')
+   
